fd805aec8d0c7cca3a047d314a06a3b751a0609a
bangladesh100 <52351110+bangladesh100@users.noreply.github.com>
2021-12-27 02:06:57 -0500

    Create food_count.py
    add files

---
+++ food_count.py
@@ -0,0 +1,17 @@
+rice, 12
+burgers, 10
+fried chicken, 9
+pie, 8
+pizza, 7
+salad,7
+tacos, 6
+avocados, 6
+bananas, 5
+ice cream, 5
+spaghetti, 5
+broccoli, 5
+fish, 4
+cake, 3
+strowbwrries, 3
+cereal, 3
+watermelon, 2
